b95dd4a9602961022895e059f7698a67720b2fd8
Simon Vadée <simon.vadee@gmail.com>
2020-10-01 17:34:19 +0200

    Fix include & revinclude bugs, enable as_json option in BundleWrapper (#6)

--- src/fhirpath/utils.py
+++ src/fhirpath/utils.py
@@ -637,8 +637,12 @@ class BundleWrapper:
 
         return link
 
-    def __call__(self):
+    def __call__(self, as_json=False):
         """ """
+        if as_json:
+            # if as_json is True, return the bundle as python dict
+            # instead of building a pydantic.BaseModel
+            return self.data
         return self.bundle_model.parse_obj(self.data)
 
     def json(self):
